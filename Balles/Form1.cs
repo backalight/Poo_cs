a016a12e4dc255d3e9a76cf8c17cb537fa1d4a1a
backalight <antoine.soros@orange.fr>
2018-11-07 13:05:45 +0100

    creation objet balle

--- Balles/Form1.cs
+++ Balles/Form1.cs
@@ -16,5 +16,7 @@ namespace Balles
         {
             InitializeComponent();
         }
+
+      
     }
 }
---
+++ Balles/PictureBall.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Balles
+{
+
+    class PictureBall : PictureBox
+    {
+        Timer tm = new Timer();
+        
+
+        public Int32 VitesseX { get; set; }
+        public Int32 VitesseY { get; set; }
+
+        public PictureBall(int VitesseX, int VitesseY)
+        {
+          
+            VitesseX = this.VitesseX;
+            VitesseY = this.VitesseY;
+            tm.Interval = 50;
+            tm.Tick += bouger;
+            tm.Enabled = true;
+        }
+
+        public PictureBall()
+        {
+            tm.Interval = 50;
+            tm.Tick += bouger;
+            tm.Enabled = true;
+        }
+    
+
+        public void bouger(Object sender, EventArgs e)
+        {
+            if (this.Left < 0) { VitesseX = -VitesseX; }
+            if (this.Left > Form.ActiveForm.Width - this.Width - 15)
+            { VitesseX = -VitesseX; }
+            if (this.Top < 0) { VitesseY = -VitesseY; }
+            if (this.Top >Form.ActiveForm.Height - this.Height - 38)
+            { VitesseY = -VitesseY; }
+            this.Left = this.Left + VitesseX;
+            this.Top = this.Top + VitesseY;
+        }
+    }
+    
+
+   
+
+}
